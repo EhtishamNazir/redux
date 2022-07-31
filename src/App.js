2992ed38964e4262ca3ff52c7f34757f4cdcb23e
EhtishamNazir <ehtishamnazir28@gmail.com>
2022-07-31 10:38:34 +0500

    uploading to github

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+
+import Counter from './components/Counter';
+import Header from './components/Header';
+import UserProfile from './components/UserProfile';
+import Auth from './components/Auth';
+import { authActions } from './store';
+
+
+function App() {
+
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
+
+  return (
+    <Fragment>
+      <Header></Header>
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
+      <Counter />
+    </Fragment>
+  );
+}
+
+export default App;
